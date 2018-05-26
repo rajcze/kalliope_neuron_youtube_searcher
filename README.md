410d11be8cfd7102d41003cbd34aad7e8cc2d543
kouzelnikCZ <radekkouzelnik@gmail.com>
2018-05-26 16:06:56 +0200

    initial commit

---
+++ README.md
@@ -0,0 +1,55 @@
+# Latest video from youtube channel
+
+## Synopsis
+
+This neuron gives you a latest video title from channel you ask for
+
+## Installation
+```bash
+kalliope install --git-url https://github.com/kouzelnikCZ/youtube_searcher.git
+```
+
+## Options
+
+
+| parameter choices | required |  comments          |   |   |
+|-------------------|----------|--------------------|---|---|
+| channel           | yes      | Name of YT channel |   |   |
+
+
+## Return Values
+
+
+| name      | description                        | type       | sample                    |
+|-----------|------------------------------------|------------|---------------------------|
+| title     | Title of latest YT video           | string     | "Démo Kalliopé FR"        |
+
+
+## Synapses example
+
+Description of what the synapse will do
+```yml
+ - name: "latest-video"
+   signals:
+     - order: "What's the latest video by {{ channel }}"
+   neurons:      
+     - youtube_searcher:
+        channel: {{ channel }}
+        file_template: youtube_searcher.j2
+    
+```
+
+## Templates example 
+
+Description of the template
+```
+{% if returncode == "Nochannelfound" %}
+    There is no such channel
+{% elif returncode == "Novideofound" %}
+    I couldn't find any video
+{% elif returncode == "Notitlefound" %}
+    I couldn't find requested title
+{% else %}
+    {{ title }}
+{% endif %}
+```
---
+++ youtube_searcher.py
@@ -0,0 +1,44 @@
+import urllib
+import re
+
+class Youtube_searcher(NeuronModule):
+def __init__(self, **kwargs):
+    cache = kwargs.get('cache', None)
+        if cache is None:
+            cache = False
+            kwargs["cache"] = cache
+
+    super(Youtube_searcher, self).__init__(**kwargs)
+    # the args from the neuron configuration
+    self.channel = kwargs.get('channel', None)
+    self.title = "None"
+    # check if parameters have been provided
+    if self._is_parameters_ok():
+        self.html_content = urllib.urlopen("https://www.youtube.com/results?sp=EgIQAg%253D%253D&search_query=" + self.channel)
+        self.html_R = self.html_content.read()
+        self.search_results = re.findall(r'href=\"/(.*?)\" class=\" yt-uix-sessionlink', self.html_R)
+        if len(self.search_results) == 0:
+            self.returncode = "Nochannelfound"
+        else:
+            self.video_url = re.findall(r'<a href=\"/watch\?v=(.{11})', "https://www.youtube.com/" + search_results[0] + "/videos")
+            if len(self.video_url) == 0:
+                self.returncode = "Novideofound"
+            else:
+                self.search_results_title = re.findall(r'\"title\":\"(.*?)\",\"', self.video_url[0])
+                if len(self.search_results_title) == 0:
+                    self.returncode = "Notitlefound"
+                else:
+                    self.title = search_results_title[0]
+
+def _is_parameters_ok(self):
+    """
+    Check if received parameters are ok to perform operations in the neuron
+    :return: true if parameters are ok, raise an exception otherwise
+
+    .. raises:: MissingParameterException
+    """
+    if self.channel is None:
+        raise MissingParameterException("You must specify a channel")
+    if not isinstance(self.channel, str):
+        raise MissingParameterException("Channel name must be a string")
+    return True
